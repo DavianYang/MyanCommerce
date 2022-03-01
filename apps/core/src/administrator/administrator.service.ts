d0f42b7ef8589f26e47c3c1edd090aaf1c7deb1c
Davian <kothantyarzarhein@gmail.com>
2022-03-01 16:20:15 +0630

    Revert "refactor(core): separate administrator module as lib"
    This reverts commit 8633c10c13f40535c51fb1b8e24af249e12d9ca5.

---
+++ apps/core/src/administrator/administrator.service.ts
@@ -0,0 +1,153 @@
+import { Injectable } from '@nestjs/common';
+
+import {
+    CreateAdministratorInput,
+    DeletionResponse,
+    DeletionResult,
+    UpdateAdministratorInput,
+} from '@myancommerce/generated';
+import { ID, PaginatedList } from '@myancommerce/nox-common';
+
+import { RequestContext } from '../api/common/request-context';
+import { TransactionalConnection } from '../connection/transactional-connection';
+import { EntityNotFoundError } from '../error/entitiy-not-found.error';
+import { User } from '../user/entities/user.entity';
+import { RoleService } from '../role/role.service';
+import { UserService } from '../user/user.service';
+import { Administrator } from './entities/administrator.entity';
+
+/**
+ * @description
+ * Contains methods relating to {@link Role} entities.
+ *
+ * @docsCategory services
+ */
+@Injectable()
+export class AdministratorService {
+    constructor(
+        private connection: TransactionalConnection,
+        private userService: UserService,
+        private roleService: RoleService,
+    ) {}
+
+    findAll(ctx: RequestContext): Promise<PaginatedList<Administrator>> {
+        return this.connection
+            .getRepository(ctx, Administrator)
+            .createQueryBuilder('administrator')
+            .getManyAndCount()
+            .then(([items, totalItems]) => ({ items, totalItems }));
+    }
+
+    findOne(
+        ctx: RequestContext,
+        administratorId: ID,
+    ): Promise<Administrator | undefined> {
+        return this.connection
+            .getRepository(ctx, Administrator)
+            .findOne(administratorId, {
+                relations: ['user', 'user.roles'],
+                where: {
+                    deletedAt: null,
+                },
+            });
+    }
+
+    findOneByUserId(
+        ctx: RequestContext,
+        userId: ID,
+    ): Promise<Administrator | undefined> {
+        return this.connection.getRepository(ctx, Administrator).findOne({
+            where: {
+                user: { id: userId },
+                deletedAt: null,
+            },
+        });
+    }
+
+    async create(
+        ctx: RequestContext,
+        input: CreateAdministratorInput,
+    ): Promise<Administrator> {
+        const administrator = new Administrator(input);
+        administrator.user = await this.userService.createAdminUser(
+            ctx,
+            input.emailAddress,
+        );
+
+        let createdAdministrator = await this.connection
+            .getRepository(ctx, Administrator)
+            .save(administrator);
+
+        for (const roleId of input.roleIds) {
+            createdAdministrator = await this.assignRole(
+                ctx,
+                createdAdministrator.id,
+                roleId,
+            );
+        }
+
+        return createdAdministrator;
+    }
+
+    async update(
+        ctx: RequestContext,
+        id: ID,
+        input: UpdateAdministratorInput,
+    ): Promise<Administrator> {
+        let administrator = await this.findOne(ctx, id);
+
+        if (!administrator) {
+            throw new EntityNotFoundError('Administrator', id);
+        }
+
+        const update = Object.assign(administrator, input);
+
+        const updatedAdministrator = this.connection
+            .getRepository(ctx, Administrator)
+            .save(update);
+
+        return updatedAdministrator;
+    }
+
+    async delete(ctx: RequestContext, id: ID): Promise<DeletionResponse> {
+        const administrator = await this.findOne(ctx, id);
+
+        if (!administrator) {
+            throw new EntityNotFoundError('Administrator', id);
+        }
+
+        await this.connection
+            .getRepository(ctx, Administrator)
+            .remove(administrator);
+
+        return {
+            result: DeletionResult.Deleted,
+        };
+    }
+
+    async assignRole(
+        ctx: RequestContext,
+        administratorId: ID,
+        roleId: ID,
+    ): Promise<Administrator> {
+        const administrator = await this.findOne(ctx, administratorId);
+
+        if (!administrator) {
+            throw new EntityNotFoundError('Administrator', administratorId);
+        }
+
+        const role = await this.roleService.findOne(ctx, roleId);
+
+        if (!role) {
+            throw new EntityNotFoundError('Role', roleId);
+        }
+
+        administrator.user.roles.push(role);
+
+        await this.connection
+            .getRepository(ctx, User)
+            .save(administrator.user, { reload: false });
+
+        return administrator;
+    }
+}
---
+++ apps/core/src/administrator/entities/administrator.entity.ts
@@ -0,0 +1,30 @@
+import { DeepPartial } from '@myancommerce/nox-common';
+import { Column, Entity, JoinColumn, OneToOne } from 'typeorm';
+import { MyanCommerceEntity } from '../../entity/base.entity';
+import { User } from '../../user/entities/user.entity';
+
+/**
+ * @description
+ * A Admin has highest level of access on MyanCommerce.
+ *
+ * @docsCategory entities
+ */
+@Entity()
+export class Administrator extends MyanCommerceEntity {
+    constructor(input?: DeepPartial<Administrator>) {
+        super(input);
+    }
+
+    @Column({ type: Date, nullable: true })
+    deletedAt: Date | null;
+
+    @Column() firstName: string;
+
+    @Column() lastName: string;
+
+    @Column({ unique: true }) emailAddress: string;
+
+    @OneToOne(() => User)
+    @JoinColumn()
+    user: User;
+}
