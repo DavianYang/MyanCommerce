04077f56ca443cae5ea4a2612e72440aa407ce97
Davian <kothantyarzarhein@gmail.com>
2022-03-11 20:47:59 +0630

    refactor(nsx-config): since using @nestjs/config, remove module

--- libs/nsx-config/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './lib/config.module';
-export * from './lib/config.service';
-export * from './lib/config.default';
-export * from './lib/merge-configs';
-export * from './lib/config.interface';
-export * from './lib/config.helpers';
