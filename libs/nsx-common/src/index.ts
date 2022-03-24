5b63e5c3b7ceb4060e1fbc63909c10b1e3699973
Davian <kothantyarzarhein@gmail.com>
2022-03-24 19:13:02 +0630

    feat(nsx-common): add common arg, enums and types
    - Add IDArgs with name parameter
    - Add Enum DeleteResult
    - Add DeleteResponsee

--- libs/nsx-common/src/index.ts
+++ libs/nsx-common/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/graphql/base.model';
 export * from './lib/graphql/language-code.enum';
 export * from './lib/graphql/common.args';
+export * from './lib/graphql/common.enums';
+export * from './lib/graphql/common.types';
