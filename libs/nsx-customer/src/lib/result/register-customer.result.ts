fc2cbb0a71db11b9642376dd4a03290fd452e0ca
Davian <kothantyarzarhein@gmail.com>
2022-04-14 17:22:10 +0630

    feat(nsx-customer): add VerifyCustomerAccountResult Dto

--- libs/nsx-customer/src/lib/result/register-customer.result.ts
+++ libs/nsx-customer/src/lib/result/register-customer.result.ts
@@ -1,14 +1,31 @@
 import { createUnionType } from '@nestjs/graphql';
 
 import { Success } from '@myancommerce/nsx-common';
-import { EmailAddressConflictError } from '@myancommerce/nsx-error';
+import {
+    EmailAddressConflictError,
+    MissingPasswordError,
+    PasswordValidationError,
+} from '@myancommerce/nsx-error';
 
 export const RegisterCustomerResult = createUnionType({
     name: 'RegisterCustomerResult',
-    types: () => [Success, EmailAddressConflictError],
+    types: () => [
+        Success,
+        EmailAddressConflictError,
+        PasswordValidationError,
+        MissingPasswordError,
+    ],
     resolveType(value) {
         if (value instanceof EmailAddressConflictError) {
             return EmailAddressConflictError;
+        }
+
+        if (value instanceof PasswordValidationError) {
+            return PasswordValidationError;
+        }
+
+        if (value instanceof MissingPasswordError) {
+            return MissingPasswordError;
         }
 
         return Success;
