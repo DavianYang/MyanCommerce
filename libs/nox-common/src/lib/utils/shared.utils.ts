73ca59ce39873d17bc58e9619c09f053cd5b9d97
Davian <kothantyarzarhein@gmail.com>
2022-04-14 17:03:03 +0630

    feat(nox-common): add assertFound method

--- libs/nox-common/src/lib/utils/shared.utils.ts
+++ libs/nox-common/src/lib/utils/shared.utils.ts
@@ -2,7 +2,16 @@ export function isObject(item: any): item is object {
     return item && typeof item === 'object' && !Array.isArray(item);
 }
 
-
 export function isClassInstance(item: any): boolean {
     return isObject(item) && item.constructor.name !== 'Object';
+}
+
+/**
+ * Identity function which asserts to the type system that a promise which can resolve to T or undefined
+ * does in fact resolve to T.
+ * Used when performing a "find" operation on an entity which we are sure exists, as in the case that we
+ * just successfully created or updated it.
+ */
+export function assertFound<T>(promise: Promise<T | undefined>): Promise<T> {
+    return promise as Promise<T>;
 }
